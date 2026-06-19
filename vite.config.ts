ad408a22db4d9a5ca6e4c3cacb902aabeee87ef0
tidinho123 <tidinhokassoma04@gmail.com>
2026-06-19 02:41:55 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@
 // You can pass additional config via defineConfig({ vite: { ... }, etc... }) if needed.
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
+base: '/ofertabonusbacbo/', 
 export default defineConfig({
   tanstackStart: {
     // Redirect TanStack Start's bundled server entry to src/server.ts (our SSR error wrapper).
